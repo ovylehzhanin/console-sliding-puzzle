4df264b751331a511ad12fc9f1b90a35bfad2f74
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2016-11-18 13:45:22 +0200

    Re-write all logic. Work prototype created

--- javascript/main.js
+++ javascript/main.js
@@ -1,76 +1,81 @@
-window.onload = function() {
+// Replace two elements in array
+Array.prototype.swap = function (a, b) {
+	this[b] = [this[a], this[a] = this[b]][0];
+};
+// Fill empty array
+Object.prototype.fill = function() {
+	for (var i = 0, deckLength = Math.pow(this.deckSize, 2) - 1; i < deckLength; i++) {
+		this.deck[i] = i + 1;
+	}	
 	
-	var cols = 4; // 
-	var testArr = prepareArray(cols);
-
-	function makeMove(e) {
-		var position = testArr.indexOf('  ');
-
-		switch(e.keyCode) {
-			case 37: // key LEFT
-				testArr.swap(position, (position + 1)%cols ? position + 1 : position);
-				break;
-			case 38: // key UP
-				testArr.swap(position, (position + 4) >= testArr.length ? position : position + 4);
-				break;
-			case 39: // key RIGHT
-				testArr.swap(position, position%cols ? position - 1 : position);
-				break;
-			case 40: // key DOWN
-				testArr.swap(position, (position - 4) < 0 ? position : position - 4);
-				break;
-			default:
-				return false;
-		}
-
-		testArr.output();
-	}
-	// Fancy output in browser console
-	Array.prototype.output = function() {
-		var separator = '---------------------------------\n';
-
-		var array = this.map(function(elem) {
-			return '\|\t' + elem + '\t\|';
-		});
-
-		for (var col = Math.sqrt(array.length), i = col -1; i < array.length; i += col) {
-			array[i] += '\n' + separator;
-		}
-
-		console.clear();
-		console.log(separator + String(array).replace(/,/g, ''));
-	}
-	// Creation of array and shuffling values
-	function prepareArray(number) {
-		var array = new Array(Math.pow(number, 2)),
-				count = array.length - 1,
-				random = 0;
-
-		for (var i = 0; i < count; i++) {
-			array[i] = i + 1;
-		}
-		array[array.length - 1] = '  ';
-		// This shuffling method is not correct
-		// (!) Need work
-		while(count) {
-			random = Math.floor(Math.random() * count);
-			array[random] = [array[count], array[count] = array[random]][0];
-
-			count--;
-		}
-
-		return array;
+	this.deck.push('  ');
+};
+// Hello 'array.swap()' :)
+Object.prototype.move = function(direction) {
+	var position = this.deck.indexOf('  ');
+	
+	switch (direction) {
+		case 37: // move right
+			(position + 1)%this.deckSize ? this.deck.swap(position, position + 1) : this.deck.swap(position, position);
+			break;
+		case 38: // move down
+			position + this.deckSize >= this.deck.length ? this.deck.swap(position, position) : this.deck.swap(position, position + this.deckSize);
+			break;
+		case 39: // move left
+			position%this.deckSize ? this.deck.swap(position, position - 1) : this.deck.swap(position, position);
+			break;
+		case 40: // move up
+			position - this.deckSize < 0 ? this.deck.swap(position, position) : this.deck.swap(position, position - this.deckSize);
+			break;
+		default:
+			break;
 	}
 
-	Array.prototype.swap = function(a, b) {
-		var temp = this[a];
-		this[a] = this[b];
-		this[b] = temp;
-	};	
-
-	window.addEventListener('keydown', makeMove, false);
-
-	testArr.output();
+	this.output();
 };
 
+Object.prototype.shuffle = function(steps) {
+	var keyCode = null;
 
+	while(steps) {
+		keyCode = Math.floor(Math.random() * 4) + 37;
+		this.move(keyCode);
+		steps--;
+	}
+};
+
+Object.prototype.output = function() {
+	var separator = '---------------------------------\n';
+
+	var array = this.deck.map(function(elem) {
+		return '\|\t' + elem + '\t\|';
+	});
+
+	for (var col = Math.sqrt(array.length), i = col -1; i < array.length; i += col) {
+		array[i] += '\n' + separator;
+	}
+
+	console.clear();
+	console.log(separator + String(array).replace(/,/g, ''));
+};
+// Function-wrapper for contex access of Object.prototype.move()
+function makeMoveFromKey(event) {
+	game.move(event.keyCode);
+}
+
+function initGame() {
+	// mysterious code here //
+}
+
+var GameSession = function(deckSize) {
+	this.deckSize = deckSize;
+	this.deck = [];
+};
+// Init part
+var game = new GameSession(4);
+
+game.fill();
+game.shuffle(400);
+game.output();
+
+window.addEventListener('keydown', makeMoveFromKey, false);
