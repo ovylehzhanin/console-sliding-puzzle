e3c6cf7775a9adf80bbfe396d1dd275fc21c7ec2
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-02-07 17:24:22 +0200

    Small changes in output() method; Create work prototype and testing  of swapArrayItems()

--- javascript/game.js
+++ javascript/game.js
@@ -9,32 +9,42 @@
 	}
 
 	game.shuffle = function() {} // ...later
+	
+	game.swapArrayItems = function(array, a, b) {
+		array[a] = [array[b], array[b] = array[a]][0];
+	}
+
+	game.makeMove = function() {} // ...later + use swapArrayItems()
 	// Render all data to console
 	game.output = function(array, colums) {
 		for (var i = 0, separator = ''; i < colums; i++) {
 			separator += '--------';
 		}
 
-		separator += '-';
+		separator += '-\n';
 
 		console.clear();
 		console.log(
-			separator + '\n' + 
-			String(
-				array.map(function(element) {
-					var lineBreak = false;
+			separator +
+			array
+			
+			.map(function(element) {
+				var lineBreak = false;
 
-					if ( !((array.indexOf(element) + 1) % colums) ) {
-						lineBreak = true;
-					}								
+				if ( !((array.indexOf(element) + 1) % colums) ) {
+					lineBreak = true;
+				}								
 
-					element = '|\t' + element + '\t|';
+				element = '|\t' + element + '\t|';
 
-					return lineBreak ?
-						element + '\n' + separator + '\n' :
-						element;
-				})
-			).replace(/,/g, '')
+				return lineBreak ?
+					element + '\n' + separator:
+					element;
+			})
+			
+			.reduce(function(emptyString, element) {
+				return emptyString + element;
+			}, '')
 		);
 	}
 	
--- javascript/main.js
+++ javascript/main.js
@@ -1,110 +1,22 @@
 (function(game) {
-
-	window.addEventListener('DOMContentLoaded', function() {
 		
-		game.data = {
-			items: [],
-			colums: 4,
-			TARGET_ITEM: ' '
-		};
-		
-		game.fill(
-			game.data.items,
-			game.data.colums,
-			game.data.TARGET_ITEM);
-
-		game.output(
-			game.data.items,
-			game.data.colums);
-
-	}, false);
+	game.data = {
+		items: [],
+		colums: 4,
+		TARGET_ITEM: ' '
+	};
+	
+	game.fill(
+		game.data.items,
+		game.data.colums,
+		game.data.TARGET_ITEM);
+	// // Test
+	// var testArr = [1, 2, 3];
+	// console.log(testArr)
+	// game.swapArrayItems(testArr, 0, 1);
+	// console.log(testArr)
+	game.output(
+		game.data.items,
+		game.data.colums);
 
 })(window.barleyBreak);
-
-
-// // Replace two elements in array
-// Array.prototype.swap = function (a, b) {
-// 	this[b] = [this[a], this[a] = this[b]][0];
-// };
-	
-// 	this.deck.push('  ');
-// };
-// // Hello 'array.swap()' :)
-// Object.prototype.move = function(direction) {
-// 	var position = this.deck.indexOf('  ');
-	
-// 	switch (direction) {
-// 		case 37: // move right
-// 			(position + 1)%this.deckSize ? this.deck.swap(position, position + 1) : this.deck.swap(position, position);
-// 			break;
-// 		case 38: // move down
-// 			position + this.deckSize >= this.deck.length ? this.deck.swap(position, position) : this.deck.swap(position, position + this.deckSize);
-// 			break;
-// 		case 39: // move left
-// 			position%this.deckSize ? this.deck.swap(position, position - 1) : this.deck.swap(position, position);
-// 			break;
-// 		case 40: // move up
-// 			position - this.deckSize < 0 ? this.deck.swap(position, position) : this.deck.swap(position, position - this.deckSize);
-// 			break;
-// 		default:
-// 			break;
-// 	}
-
-// 	this.output();
-// };
-
-// Object.prototype.shuffle = function(steps) {
-// 	var keyCode = null;
-
-// 	while(steps) {
-// 		keyCode = Math.floor(Math.random() * 4) + 37;
-// 		this.move(keyCode);
-// 		steps--;
-// 	}
-// };
-
-// Object.prototype.output = function() {
-// 	var separator = '',
-// 		col = this.deckSize;
-
-// 	for (var i = 0; i < col; i++) {
-// 		separator += '--------';
-// 	}
-
-// 	separator += '-';
-
-// 	var array = this.deck.map(function(elem) {
-// 		return '\|\t' + elem + '\t\|';
-// 	});
-
-// 	for (var j = col -1; j < array.length; j += col) {
-// 		array[j] += '\n' + separator + '\n';
-// 	}
-
-// 	console.clear();
-// 	console.log(separator + '\n' + String(array).replace(/,/g, ''));
-// };
-
-// var GameSession = function(deckSize) {
-// 	this.deckSize = deckSize;
-// 	this.deck = [];
-// 	this.makeMoveFromKey = function(event) {
-// 		this.move(event.keyCode);
-// 	}
-// };
-
-// window.onload = function() {
-// 	var generate = document.getElementById('generate');
-
-// 	var initGame = function() {
-// 		var game = new GameSession(Number(document.getElementById('setDeckSize').value));
-	
-// 		game.fill();
-// 		game.shuffle(400);
-// 		game.output();
-	
-// 		window.addEventListener('keydown', game.makeMoveFromKey.bind(game), false);
-// 	}
-// 	console.log('Hey buddy!:)\nChoose your deck size and press "Generate"');
-// 	generate.addEventListener('click', initGame, false);
-// };
