e699e292ea0563cbe68fef6d8cbe07038ab65167
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2017-10-23 23:18:57 +0300

    Add constats

---
+++ source/javascript/constants.js
@@ -0,0 +1,15 @@
+// One-space character for empty item
+const SPACE = ' ';
+
+// Box Drawing characters:
+// https://en.wikipedia.org/wiki/Box-drawing_character
+const U = {
+  '2550' : '═',
+  '2551' : '║',
+  '2554' : '╔',
+  '2557' : '╗',
+  '255A' : '╚', 
+  '255D' : '╝'
+}; 
+
+export { U }
