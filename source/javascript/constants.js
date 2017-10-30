ca31128a881db8b1608ceea2096b48b9c7c0c6c2
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2017-10-30 22:26:26 +0200

    Test rendering

--- source/javascript/constants.js
+++ source/javascript/constants.js
@@ -1,7 +1,8 @@
 import { _sfcc } from './extensions'
 
 // One-space character for empty item
-const SPACE_CODE = '0x0020';
+const SPACE_CODE = '0x0020',
+  SPACE = _sfcc(SPACE_CODE);
 
 // Unicode 'Box Drawing' characters:
 // https://en.wikipedia.org/wiki/Box-drawing_character
@@ -19,7 +20,8 @@ const DRAW_DATA = {
   DECK: {
     HL: '0x2550', VL: '0x2551',
     TL: '0x2554', TR: '0x2557',
-    BL: '0x255A', BR: '0x255B'
+    BL: '0x255A', BR: '0x255B',
+    lineWidth: 26
   },
   // ****
   // Default item borders
@@ -27,10 +29,18 @@ const DRAW_DATA = {
   ITEM: {
     HL: '0x2501', VL: '0x2503',
     TL: '0x250F', TR: '0x2513',
-    BL: '0x2517', BR: '0x251B'
+    BL: '0x2517', BR: '0x251B',
+    lineWidth: 4
+  },
+
+  ITEM_EMPTY: {
+    HL: SPACE_CODE, VL: SPACE_CODE,
+    TL: SPACE_CODE, TR: SPACE_CODE,
+    BL: SPACE_CODE, BR: SPACE_CODE,
+    lineWidth: 4
   }
 }; 
 
 const TARGET_ITEM = _sfcc(SPACE_CODE);
 
-export { DRAW_DATA, TARGET_ITEM }
+export { DRAW_DATA, TARGET_ITEM, SPACE }
