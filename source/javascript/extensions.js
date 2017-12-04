aacd1791b122e394c92d49c402c3718b0e1cb086
Oleksii Vylehzhanin <o.vylehzhanin@sana-commerce.com>
2017-12-04 12:38:02 +0200

    Add custom 'repeat' method for array.prototype

--- source/javascript/extensions.js
+++ source/javascript/extensions.js
@@ -1,11 +1,21 @@
 // Replace two item indexes in array
-Array.prototype._swap = function(a, b) {
+Array.prototype._swap = function (a, b) {
   this[a] = [this[b], this[b] = this[a]][0];
 };
 
 // Replace item value at 'index'
 function _replace(array, index, newItemValue) {
   // add code here...
+}
+
+Array.prototype._repeat = function (size) {
+  let result = [];
+
+  for (let i = 0; i < size; i += 1) {
+    result = result.concat(this);
+  }
+
+  return result;
 }
 
 function _arrayRepeat(value, size) {
