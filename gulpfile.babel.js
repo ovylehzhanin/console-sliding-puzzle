795bdcbfb327005b26b65ebacfd8973fedc1ede3
Oleksii Vylehzhanin <o.vylehzhanin@sana-commerce.com>
2017-12-08 10:19:39 +0200

    add js uglify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,16 @@
 import gulp from 'gulp';
 import pug from 'gulp-pug';
 import browserify from 'browserify';
+import uglify from 'gulp-uglify';
 import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
 import browserSync from 'browser-sync';
 
 let sync = browserSync.create();
 
 gulp.task('pug', () => {
   return gulp.src('./source/index.pug')
-    .pipe(pug({
-      pretty: true
-    }))
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('./dist'));
 });
 
@@ -19,23 +19,20 @@ gulp.task('js-build', () => {
     .transform('babelify')
     .bundle()
     .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
 
-
 gulp.task('static-server', ['pug', 'js-build'], done => {
-  sync.init({
-    server: './dist'  
-  });
+  sync.init({ server: './dist' });
   done();
 });
-
 
 gulp.task('reload', ['pug', 'js-build'], done => {
   sync.reload();
   done();
 });
-
 
 gulp.task('watcher', ['static-server'], () => {
   gulp.watch(['./source**.*pug', './source/javascript/**.*js'], ['pug', 'js-build', 'reload']);
