359c9fdedcbbc8a30cd1ba08c34bc10ed853a670
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2017-12-12 23:06:06 +0200

    replace direction parse in separate method; add test-shuffling

--- source/javascript/controller.js
+++ source/javascript/controller.js
@@ -13,34 +13,46 @@ class Controller {
     };
   }
 
+  _getDirectionFromKeyCode(keyCode, keys) {
+    switch(keyCode) {
+      case keys.LEFT:
+        return 'right';
+
+      case keys.UP:
+        return 'down';
+
+      case keys.RIGHT:
+        return 'left';
+
+      case keys.DOWN:
+        return 'top';
+
+      defalut:
+        break;
+    }
+  }
+
   _moveItems(event) {
     let keyCode = event.keyCode,
       direction = '';
 
     if (keyCode >= KEYS.LEFT && keyCode <= KEYS.DOWN) {
-      switch(keyCode) {
-        case KEYS.LEFT:
-          direction = 'right';
-          break;
-
-        case KEYS.UP:
-          direction = 'down';
-          break;
-
-        case KEYS.RIGHT:
-          direction = 'left';
-          break;
-
-        case KEYS.DOWN:
-          direction = 'top';
-          break;
-
-        default:
-          break;
-      }
-
+      direction = this._getDirectionFromKeyCode(keyCode, KEYS);
       this._model.replaceItems(direction);
       this._render.render(this._model.getItems(), this._model.targetItemPosition);
+      this._model._checkCompletion();
+    }
+  }
+
+  _shuffleItems() {
+    let iterations = 1000,
+      simulatedKeyCode = null,
+      simulatedDirection = '';
+
+    for (let i = 0; i < iterations; i += 1) {
+      simulatedKeyCode = Math.floor((Math.random() * 4) + 37);
+      simulatedDirection = this._getDirectionFromKeyCode(simulatedKeyCode, KEYS);
+      this._model.replaceItems(simulatedDirection);
     }
   }
 
@@ -49,8 +61,10 @@ class Controller {
   }
 
   init() {
+    this._shuffleItems();
     this._model._findPossibleMoves();
     this._render.init(DRAW_DATA, this._model.matrixSize);
+    this._render.render(this._model.getItems(), this._model.targetItemPosition);
     this._bindEvents();
   }
 }
