ec97eaddd81ab421602300969ec19ec0b1951697
Oleksii Vylehzhanin <a.a.vylegzhanin@gmail.com>
2020-04-15 01:14:38 +0300

    wip: complete minimal setup for js build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: '',
+  entry: './source/javascript/main.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, './public', 'scripts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
   }
 };
